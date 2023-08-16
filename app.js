d26258352a1efcf3c9205591ae7ef30de3cde30c
ketankapale8 <ketan.kapale@gmail.com>
2023-08-16 21:44:54 +0530

    changes

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
-app.use(cors(corsOptions))
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true , limit: '50mb'}))
