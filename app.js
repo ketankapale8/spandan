7485f9975f25e42653b23e9cf8ec0791c75779fe
ketankapale8 <ketan.kapale@gmail.com>
2023-08-16 22:25:44 +0530

    c

--- app.js
+++ app.js
@@ -9,21 +9,17 @@ import NadiRouter from './routes/nadi.js';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 import cors from 'cors'
-
 export const app = express();
+
 const corsOptions ={
     origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:false,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
-app.use(cors())
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-    });
-
+ 
+ 
+    
+app.use(cors(corsOptions))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true , limit: '50mb'}))
 app.use(cookieParser());
